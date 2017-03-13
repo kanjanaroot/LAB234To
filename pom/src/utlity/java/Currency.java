b6d115defcbe4ebbf8aa5bb72466f0e4558ffd75
DESKTOP-11156IH\ASUS <kanjanaroot_K@cmu.ac.th>
2017-03-13 18:20:45 +0700

    2.6

--- pom/src/utlity/java/Currency.java
+++ pom/src/utlity/java/Currency.java
@@ -2,7 +2,66 @@
  * Created by ASUS on 13/03/2017.
  */
 public class Currency {
-    public double exchange(String sourceCurrency, double amount, String targetCurrency){
-        return 0.0;
+    String[] currency = {"USD","EUR","THB"};
+
+    public double exchange (String sourceCurrency, double amount, String targetCurrency){
+
+        OUTER:
+        {
+            for (int i = 0; i < currency.length; i++) {
+                if (sourceCurrency == currency[i]) break OUTER;
+            }
+            return Double.NaN;
+        }
+
+        OUTER:
+        {
+            for (int i = 0; i < currency.length; i++) {
+                if (targetCurrency == currency[i]) break OUTER;
+            }
+            return Double.NaN;
+        }
+
+        if(sourceCurrency == targetCurrency){
+            return amount;
+        }else if (sourceCurrency == "USD"){
+            return sourceIsUSD(sourceCurrency,amount,targetCurrency);
+        }else if (targetCurrency == "USD"){
+            return targetIsUSD(sourceCurrency,amount,targetCurrency);
+        }else{
+            return bothIsNotUSD(sourceCurrency,amount,targetCurrency);
+        }
+    }
+
+    private double targetIsUSD (String sourceCurrency, double amount, String targetCurrency){
+        if(sourceCurrency=="THB"){
+            amount = amount/33.0;
+            return amount;
+        }else{
+            amount = amount / 1.01;
+            return amount;
+        }
+    }
+
+    private double bothIsNotUSD (String sourceCurrency, double amount, String targetCurrency) {
+        if (sourceCurrency == "THB") {
+            amount = amount / 33.0;
+            amount = amount * 1.01;
+            return amount;
+        }else {
+            amount = amount / 1.01;
+            amount = amount * 33;
+            return amount;
+        }
+    }
+
+    private double sourceIsUSD (String sourceCurrency, double amount, String targetCurrency){
+        if(targetCurrency=="THB"){
+            amount = amount * 33.0;
+            return amount;
+        }else{
+            amount = amount * 1.01;
+            return amount;
+        }
     }
 }
